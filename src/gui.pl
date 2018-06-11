fdb84c09e0ab2b0c54bbdf2906864ea3da52ee1f
Przemyslaw Kobylanski <przemko@mac.com>
2018-06-11 14:44:15 +0200

    poprawki

--- src/gui.pl
+++ src/gui.pl
@@ -23,38 +23,20 @@ draw([Cell | Row], Window, Y, X) :-
 draw_cell(Cell, _, _, _) :-
     var(Cell), !.
 draw_cell([North, East, South, West], Window, Y, X) :-
-    draw_north(North, Window, Y, X),
-    draw_east(East, Window, Y, X),
-    draw_south(South, Window, Y, X),
-    draw_west(West, Window, Y, X).
+    draw_triangle(North, Window, X,Y, X+40,Y, X+20,Y+20),
+    draw_triangle(East,  Window, X+40,Y, X+40,Y+40, X+20,Y+20),
+    draw_triangle(South, Window, X+20,Y+20, X+40,Y+40, X,Y+40),
+    draw_triangle(West,  Window, X,Y, X+20,Y+20, X,Y+40).
 
-draw_north(North, _, _, _) :-
-    var(North), !.
-draw_north(+, _, _, _) :- !.
-draw_north(North, Window, Y, X) :-
-    color(North, Color),
-    triangle(Color, Window, Y,X, Y,X+40, Y+20,X+20).
-
-draw_east(East, _, _, _) :-
-    var(East), !.
-draw_east(+, _, _, _) :- !.
-draw_east(East, Window, Y, X) :-
-    color(East, Color),
-    triangle(Color, Window, Y,X+40, Y+40,X+40, Y+20,X+20).
-
-draw_south(South, _, _, _) :-
-    var(South), !.
-draw_south(+, _, _, _) :- !.
-draw_south(South, Window, Y, X) :-
-    color(South, Color),
-    triangle(Color, Window, Y+20,X+20, Y+40,X+40, Y+40,X).
-
-draw_west(West, _, _, _) :-
-    var(West), !.
-draw_west(+, _, _, _) :- !.
-draw_west(West, Window, Y, X) :-
-    color(West, Color),
-    triangle(Color, Window, Y,X, Y+20,X+20, Y+40,X).
+draw_triangle(Pc, Window, X1,Y1, X2,Y2, X3,Y3) :-
+    color(Pc, Color),
+    new(Path, path(poly)),
+    send(Path, append, point(X1, Y1)),
+    send(Path, append, point(X2, Y2)),
+    send(Path, append, point(X3, Y3)),
+    send(Path, fill_pattern, colour(Color)),
+    send(Path, colour, colour(black)),
+    send(Window, display, Path).
 
 color(1, yellow).
 color(2, orange).
@@ -63,15 +45,3 @@ color(4, green).
 color(5, blue).
 color(6, gray).
 color(7, magenta).
-
-
-triangle(C, W, Y1,X1, Y2,X2, Y3,X3) :-
-    new(P, path(poly)),
-    send(P, append, point(X1, Y1)),
-    send(P, append, point(X2, Y2)),
-    send(P, append, point(X3, Y3)),
-    send(P, fill_pattern, colour(C)),
-    send(P, colour, colour(black)),
-    send(W, display, P).
-
-
